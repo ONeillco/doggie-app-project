d264b9575b65538d991f74e210cac3d3333dfb4e
Cory <oneillco117@gmail.com>
2021-09-05 17:44:24 -0400

    updated crud

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Nav from './components/Nav';
+import DogList from './components/DogList';
+import DeedList from './components/DeedList';
+import Home from './components/Home';
+import ErrorPage from './components/ErrorPage';
+import AddDog from './AddDog';
+import EditDog from './components/EditDog';
+// import DogDetails from './components/DogDetails';
+
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Nav />
+      <Switch>
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/dogs" component={DogList} />
+        <Route exact path="/dogs/add" component={AddDog} />
+        <Route exact path="/dogs/:id/edit" component={EditDog} />
+        {/* <Route exact path="/dogs/:id" component={DogDetails} /> */}
+        <Route exact path="/deeds" component={DeedList} />
+        <Route component={ErrorPage} />
+
+     </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
